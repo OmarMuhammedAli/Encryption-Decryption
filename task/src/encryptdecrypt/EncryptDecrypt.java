971636a7d4625cb5a3bf0f4b2cd9f4b77b0118cc
Omar <omarmuhammed1998.om@gmmail.com>
2020-07-12 03:39:29 +0200

    Supports upper case now

--- task/src/encryptdecrypt/EncryptDecrypt.java
+++ task/src/encryptdecrypt/EncryptDecrypt.java
@@ -3,6 +3,8 @@ package encryptdecrypt;
 public class EncryptDecrypt {
     final String LETTERS ="abcdefghijklmnopqrstuvwxyz";
     final String REVERSED_LETTERS = "zyxwvutsrqponmlkjihgfedcba";
+    final String UPPER_CASE = LETTERS.toUpperCase();
+    final String REVERSED_UPPER_CASE = REVERSED_LETTERS.toUpperCase();
     private StringBuilder entry = new StringBuilder();
 
     public EncryptDecrypt(){}
@@ -18,7 +20,11 @@ public class EncryptDecrypt {
     public StringBuilder reversedLettersEncryption() {
         for (int i = 0; i < entry.length(); i++) {
             if (Character.isLetter(entry.charAt(i))) {
-                entry.setCharAt(i, REVERSED_LETTERS.charAt(LETTERS.indexOf(entry.charAt(i))));
+                if (Character.isLowerCase(entry.charAt(i))){
+                    entry.setCharAt(i, REVERSED_LETTERS.charAt(LETTERS.indexOf(entry.charAt(i))));
+                } else {
+                    entry.setCharAt(i, REVERSED_UPPER_CASE.charAt(UPPER_CASE.indexOf(entry.charAt(i))));
+                }
             }
         }
         return this.entry;
@@ -29,11 +35,19 @@ public class EncryptDecrypt {
         int dupKey = key;
         for (int i = 0; i < entry.length(); i++){
             if (Character.isLetter(entry.charAt(i))) {
-                while ((LETTERS.indexOf(entry.charAt(i)) + dupKey) > 25) {
-                    dupKey -= 26;
+                if (Character.isLowerCase(entry.charAt(i))) {
+                    while ((LETTERS.indexOf(entry.charAt(i)) + dupKey) > 25) {
+                        dupKey -= 26;
+                    }
+                    entry.setCharAt(i, LETTERS.charAt(LETTERS.indexOf(entry.charAt(i)) + dupKey));
+                    dupKey = key;
+                } else {
+                    while ((UPPER_CASE.indexOf(entry.charAt(i)) + dupKey) > 25) {
+                        dupKey -= 26;
+                    }
+                    entry.setCharAt(i, UPPER_CASE.charAt(UPPER_CASE.indexOf(entry.charAt(i)) + dupKey));
+                    dupKey = key;
                 }
-                entry.setCharAt(i, LETTERS.charAt(LETTERS.indexOf(entry.charAt(i)) + dupKey));
-                dupKey = key;
             }
         }
         return entry;
